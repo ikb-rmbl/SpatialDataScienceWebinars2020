ccbec094e23f1c2220227f6e0e4a857ad2ed2583
Ian Breckheimer <ian@Ians-MacBook-Air.local>
2020-09-21 10:33:53 -0600

    Initial commit of demo scripts.

---
+++ Webinar1_SDP_Intro/code/access_SDP_cloud_data.R
@@ -0,0 +1,131 @@
+##Script to demo raster remote access to SDP Data Products stored 
+##in Amazon S3.
+
+##Author: Ian Breckheimer
+##Updated: 9-20-2020
+
+## Installs packages
+#install.packages(c("rgdal","raster","sf","ggplot2","ggspatial","rasterVis"))
+
+##Sets up workspace
+library(rgdal)
+library(raster)
+library(sf)
+library(ggplot2)
+library(ggspatial)
+library(rasterVis)
+
+##Project directory
+proj_dir <- "~/code/SpatialDataScienceWebinars2020/"
+
+##Sets working directory.
+setwd(proj_dir)
+
+##Check GDAL version (should work with GDAL 2.2.3 or later).
+getGDALVersionInfo()
+
+##Get data frome with all available SDP data products.
+sdp_prods <- read.csv("https://www.rmbl.org/wp-content/uploads/2020/06/SDP_product_table_6_8_2020.csv")
+View(sdp_prods)
+
+##Creates raster objects from cloud-based datasets.
+dem_uri <- as.character(sdp_prods$Data.URL[sdp_prods$Product=="Digital Elevation Model"])
+dem_path <- paste("/vsicurl/",dem_uri,sep="")
+dem <- raster(dem_path, progress='text')
+dem
+
+flow_uri <- as.character(sdp_prods$Data.URL[sdp_prods$Product=="Multi-direction Flow Accumulation"])
+flow_path <- paste("/vsicurl/",flow_uri,sep="")
+flow <- raster(flow_path,progress='text')
+flow
+
+water_uri <- as.character(sdp_prods$Data.URL[sdp_prods$Product=="Surface Water Cover"])
+water_path <- paste("/vsicurl/",water_uri,sep="")
+water <- raster(water_path,progress='text')
+water
+
+##Combines them into a raster stack.
+dem_flow <- stack(dem,flow,water)
+names(dem_flow) <- c("dem","flow","water")
+
+##Gothic Townsite Extent
+#plot(dem,maxpixels=5000)
+#gothic_extent <- drawExtent()
+gothic_extent <- extent(matrix(c(326876,328551,
+                                 4312941,4314552),
+                               nrow=2,byrow=TRUE))
+
+##Subsets rasters to the area of interest.
+gothic_stack <- crop(dem_flow, gothic_extent, filename=tempfile(), 
+                   progress="text")
+
+##computes derived functions of subset maps, adding output to stack
+gothic_stack$slope <- terrain(gothic_stack$dem,opt="slope",
+                              progress="text",filename=tempfile())
+gothic_stack$aspect <- terrain(gothic_stack$dem,opt="aspect",
+                              progress="text",filename=tempfile())
+
+##Doing arbitrary arithmetic on raster layers
+##does raster algebra, creating a new map in memory if it's small
+gothic_stack$flow_log <- log(gothic_stack$flow)
+inMemory(gothic_stack$flow_log)
+
+##plots all layers
+plot(gothic_stack)
+
+##samples values at random points where all layers have data
+gothic_samples <- sampleRandom(gothic_stack,size=30,
+                               sp=TRUE,na.rm=TRUE)
+##adds coordinates to data frame.
+gothic_samples$east <- coordinates(gothic_samples)[,1]
+gothic_samples$north <- coordinates(gothic_samples)[,2]
+
+##computes a hillshade layer for plotting
+gothic_stack$hillshade <- hillShade(slope=gothic_stack$slope,
+                                    aspect=gothic_stack$aspect,
+                                    filename=tempfile())
+
+##converts water data to polygons.
+water_poly <- st_as_sf(rasterToPolygons(gothic_stack$water))
+flow_poly <- st_as_sf(rasterToPolygons(gothic_stack$flow_log,fun=function(x){x>11}))
+
+##plots the hillshade and adds sampling points
+plot(gothic_stack$hillshade,ext=gothic_extent,
+     main="Gothic Sample Points",
+     col=hcl.colors(n=50,palette="Grays",rev=FALSE))
+plot(water_poly,add=TRUE,col="slateblue")
+plot(flow_poly,add=TRUE,col="slateblue")
+points(gothic_samples,pch="+",cex=1,col="white")
+
+##makes a prettier plot with ggplot2, rasterVis, and ggspatial
+gplot <- gplot(gothic_stack$hillshade,maxpixels=500000)+
+  geom_raster(aes(fill=value),interpolate=TRUE)+
+  scale_fill_gradient(low = 'black', high = 'white',
+                      guide = guide_none()) +
+  layer_spatial(water_poly,aes(color="water"))+
+  layer_spatial(flow_poly,color="slateblue")+
+  geom_point(aes(x=east,y=north,color="sites"),
+             data=gothic_samples@data,
+             shape="+",size=5)+
+  scale_color_manual("Legend", guide = "legend",
+                     values = c("water" = "slateblue",
+                                "sites" = "white")) +
+  scale_x_continuous("")+
+  scale_y_continuous("")+
+  coord_sf(expand=0, label_axes="--EN") +
+  annotation_scale(style="ticks",width_hint=0.1,location="br",
+                   tick_height=0,text_col="white",line_col="white")+
+  guides(color = guide_legend(override.aes = list(fill=c("grey80","slateblue"),
+                                                  linetype = c(0, 0),
+                                                  shape = c(43, 15))))+
+  theme_bw()+
+  theme(legend.position=c(0.85,0.85))
+
+##Writes plot to disk.
+png("stream_sample_map.png",width=6,height=8,units="in",res=300)
+gplot
+dev.off()
+
+##Writes subset of raster data to disk.
+writeRaster(gothic_stack,filename="Gothic_Townsite_Rasters.tif",
+            progress='text')
---
+++ Webinar1_SDP_Intro/code/process_SDP_cloud_data_parallel.R
@@ -0,0 +1,100 @@
+##Script to demo parallel processing of SDP Data Products stored 
+##in the cloud.
+
+##Sets up workspace
+library(rgdal)
+library(raster)
+library(sp)
+library(TileManager)
+library(foreach)
+library(progress)
+
+##need to install Rtools before installing this package on windows.
+library(doParallel)
+
+##Project directory
+proj_dir <- "~/code/SpatialDataScienceWebinars2020/"
+
+##Sets working directory.
+setwd(proj_dir)
+
+##Sets raster options
+rasterOptions(maxmemory=8e+9,memfrac=0.8)
+
+##creates a raster object from a cloud-based source
+##Get data frome with all available SDP data products.
+sdp_prods <- read.csv("https://www.rmbl.org/wp-content/uploads/2020/06/SDP_product_table_6_8_2020.csv")
+View(sdp_prods)
+
+##Creates raster objects from cloud-based datasets.
+dem_uri <- as.character(sdp_prods$Data.URL[sdp_prods$Product=="Digital Elevation Model"])
+dem_path <- paste("/vsicurl/",dem_uri,sep="")
+dem <- raster(dem_path, progress='text')
+dem
+
+flow_uri <- as.character(sdp_prods$Data.URL[sdp_prods$Product=="Multi-direction Flow Accumulation"])
+flow <- raster(paste(basepath,"UER_flow_accum_mfd_1m_v1.tif",sep=""),
+               progress='text')
+
+##study area of interest
+aoi_extent <- extent(matrix(c(325876,328251,
+                                 4312941,4314552),
+                               nrow=2,byrow=TRUE))
+
+##downloads a local copy of the data for the area of interest
+local_brick <- crop(stack(dem,flow),y=aoi_extent,progress='text')
+names(local_brick) <- c("dem","flow")
+inMemory(local_brick)
+
+
+##computes a slope layer without tiling or parallelization.
+##this is much faster for small areas.
+start_time <- Sys.time()
+dem_slope <- terrain(local_brick$dem,opt="slope",
+                     progress="text")
+end_time <- Sys.time()
+
+total_elapsed <- end_time - start_time
+total_elapsed
+
+##set up the tiling scheme for processing larger areas.
+tiles <- TileScheme(dem,dimByDist=500,buffer=5)
+
+##create and register a parallel backend.
+#cl <- makeCluster(parallel::detectCores()-2,outfile="")
+cl <- makeCluster(detectCores()/2,outfile="")
+registerDoParallel(cl)
+
+##create a progress bar
+#pb <- txtProgressBar(title = "Iterative training", min = 0, max = 30, style = 3)
+
+##creates a parallel foreach loop to process each tile.
+start_time <- Sys.time()
+out_tiles <- foreach(i=1:length(tiles$buffPolygons),
+                     .packages=c("raster")) %dopar% {
+  
+  tile_extent <- extent(bbox(tiles$buffPolygons[i,]))
+  tile <- crop(dem,tile_extent)
+  tile_slope <- terrain(tile,opt="slope",
+                        progress="text")
+  out_extent <- extent(bbox(tiles$nbuffPolygons[i,]))
+  tile_filename <- 
+  tile_slope_crop <- crop(tile_slope,out_extent)
+  
+  (tile_slope_crop)
+}
+stopCluster(cl)
+
+end_time <- Sys.time()
+
+total_elapsed <- end_time - start_time
+total_elapsed
+
+##mosaics the resulting tiles.
+out_tiles$ext <- aoi_extent
+out_tiles$filename="dem_slope_merged.tif"
+out_tiles$overwrite=TRUE
+slope_merged <- do.call(merge,out_tiles)
+
+plot(slope_merged)
+
