042da95918cbac8ec3d00ad56afa9a4f0e6e3c81
Ian Breckheimer <ian@ians-air.wsc.western.edu>
2020-09-22 08:44:34 -0600

    Added scale visualization.

--- Webinar1_SDP_Intro/code/access_SDP_cloud_data.R
+++ Webinar1_SDP_Intro/code/access_SDP_cloud_data.R
@@ -5,7 +5,7 @@
 ##Updated: 9-20-2020
 
 ## Installs packages
-#install.packages(c("rgdal","raster","sf","ggplot2","ggspatial","rasterVis"))
+#install.packages(c("rgdal","raster","sf","ggplot2","ggspatial","rasterVis","gridExtra"))
 
 ##Sets up workspace
 library(rgdal)
@@ -14,6 +14,7 @@ library(sf)
 library(ggplot2)
 library(ggspatial)
 library(rasterVis)
+library(gridExtra)
 
 ##Project directory
 proj_dir <- "~/code/SpatialDataScienceWebinars2020/"
@@ -51,8 +52,8 @@ names(dem_flow) <- c("dem","flow","water")
 ##Gothic Townsite Extent
 #plot(dem,maxpixels=5000)
 #gothic_extent <- drawExtent()
-gothic_extent <- extent(matrix(c(326876,328551,
-                                 4312941,4314552),
+gothic_extent <- extent(matrix(c(326983,328033,
+                                 4313306,4314244),
                                nrow=2,byrow=TRUE))
 
 ##Subsets rasters to the area of interest.
@@ -66,7 +67,7 @@ gothic_stack$aspect <- terrain(gothic_stack$dem,opt="aspect",
                               progress="text",filename=tempfile())
 
 ##Doing arbitrary arithmetic on raster layers
-##does raster algebra, creating a new map in memory if it's small
+##does raster algebra, creating a new raster map in memory if it's small
 gothic_stack$flow_log <- log(gothic_stack$flow)
 inMemory(gothic_stack$flow_log)
 
@@ -76,16 +77,61 @@ plot(gothic_stack)
 ##samples values at random points where all layers have data
 gothic_samples <- sampleRandom(gothic_stack,size=30,
                                sp=TRUE,na.rm=TRUE)
-##adds coordinates to data frame.
-gothic_samples$east <- coordinates(gothic_samples)[,1]
-gothic_samples$north <- coordinates(gothic_samples)[,2]
 
 ##computes a hillshade layer for plotting
 gothic_stack$hillshade <- hillShade(slope=gothic_stack$slope,
                                     aspect=gothic_stack$aspect,
                                     filename=tempfile())
+plot(gothic_stack$hillshade,)
+points(gothic_samples)
 
-##converts water data to polygons.
+##resamples rasters to a coarser resolution
+template_100m <- raster(crs=proj4string(gothic_stack),ext=extent(gothic_stack),resolution=100)
+dem_100m <- resample(gothic_stack$dem,y=template_100m,method="bilinear")
+slope_100m <- resample(gothic_stack$slope,y=template_100m, method="bilinear")
+flow_log_100m <- resample(gothic_stack$flow_log,y=template_100m,method="bilinear")
+coarse_stack <- stack(dem_100m,slope_100m,flow_log_100m)
+names(coarse_stack) <- c("dem_100m","slope_100m","flow_log_100m")
+
+##Plots resampled data.
+plot(coarse_stack)
+
+##samples values with the resampled data.
+gothic_samples <- extract(coarse_stack,gothic_samples,method="bilinear",sp=TRUE)
+
+##adds coordinates to data frame.
+gothic_samples$east <- coordinates(gothic_samples)[,1]
+gothic_samples$north <- coordinates(gothic_samples)[,2]
+
+##plots original vs resampled data.
+p1 <- ggplot(gothic_samples@data)+
+  geom_point(aes(x=dem,y=dem_100m))+
+  geom_abline(aes(intercept=0,slope=1))+
+  ggtitle("Elevation")+
+  xlab("1m-Resolution DEM")+
+  ylab("100m-Resolution DEM")+
+  theme_bw()
+
+p2 <- ggplot(gothic_samples@data)+
+  geom_point(aes(x=slope,y=slope_100m))+
+  geom_abline(aes(intercept=0,slope=1))+
+  ggtitle("Slope")+
+  xlab("1m-Resolution Slope")+
+  ylab("100m-Resolution Slope")+
+  theme_bw()
+
+p3 <- ggplot(gothic_samples@data)+
+  geom_point(aes(x=flow_log,y=flow_log_100m))+
+  geom_abline(aes(intercept=0,slope=1))+
+  ggtitle("Log Flow Accum.")+
+  xlab("1m-Resolution Flow")+
+  ylab("100m-Resolution Flow")+
+  theme_bw()
+
+gridExtra::grid.arrange(p1,p2,p3,ncol=3)
+
+
+##converts water data to polygons (this is slow).
 water_poly <- st_as_sf(rasterToPolygons(gothic_stack$water))
 flow_poly <- st_as_sf(rasterToPolygons(gothic_stack$flow_log,fun=function(x){x>11}))
 
