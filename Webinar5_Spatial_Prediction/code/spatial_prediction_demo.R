7e3f0bfb507a95f869bddf7036e5eaf650b1948b
Ian Breckheimer <ian@ians-air-2.lan>
2021-03-30 07:43:27 -0600

    Squashed a few file path bugs and added ensemble comparison to the demo script.

--- Webinar5_Spatial_Prediction/code/spatial_prediction_demo.R
+++ Webinar5_Spatial_Prediction/code/spatial_prediction_demo.R
@@ -3,14 +3,12 @@
 ####Updated: 29 March 2021
 
 #### Set up workspace ####
-install.packages(c("raster","rgdal","sf","readr",
-                   "dplyr","tibble","tidymodels",
-                   "parsnip","tidymodels","keras",
-                   "xgboost","kernlab","mgcv",
-                   "foreach","future","doFuture",
-                   "TileManager","foreach","progress",
-                   "doParallel","RhpcBLASctl","gdalUtils"))
-
+# install.packages(c("raster","rgdal","sf","readr",
+#                    "dplyr","tibble","tidymodels",
+#                    "parsnip","xgboost","kernlab","mgcv",
+#                    "foreach","future","doFuture",
+#                    "TileManager","foreach","progress",
+#                    "doParallel","RhpcBLASctl","gdalUtils"))
 
 #### Assemble and extract features ####
 library(raster)
@@ -70,11 +68,10 @@ decid_labeled$Blue_norm_2019 <- decid_labeled$UG_rgbn_sept2019_1m_v1.3 / (decid_
 decid_labeled$NIR_norm_2019 <- decid_labeled$UG_rgbn_sept2019_1m_v1.4 / (decid_labeled$Brightness_2019 * 255 * 4)
 
 ##splits into training and testing sets.
-##Splits into training, testing and validation.
 set.seed(42)
 split_id <- runif(nrow(decid_labeled),0,1)
-decid_train <- decid_labeled[split_id <= 0.7,]
-decid_test <- decid_labeled[split_id > 0.7,]
+gam_train <- decid_labeled[split_id <= 0.7,]
+gam_test <- decid_labeled[split_id > 0.7,]
 
 ##Fits a GAM model.
 decid_gam <- gam(Train_Decid ~ s(Red_norm_2017,Red_norm_2019)+
@@ -83,24 +80,25 @@ decid_gam <- gam(Train_Decid ~ s(Red_norm_2017,Red_norm_2019)+
                    s(UG_ndvi_oct2017_1m_v1,UG_ndvi_sept2019_1m_v2)+
                    s(Brightness_2017,Brightness_2019)+
                    s(UG_canopy_height_1m_v2),
-                 family="binomial",data=decid_train)
+                 family="binomial",data=gam_train)
 summary(decid_gam)
 
-##Writes the model to disk.
-saveRDS(decid_gam,"./Webinar5_Spatial_Prediction/output/gam_fit.RDS")
-
 ##Predicts on training and held-out data.
-decid_test$gam_pred <- predict(decid_gam,newdata=decid_test,type="response")
-decid_test$gam_class <- decid_test$gam_pred > 0.5
-decid_train$gam_pred <- predict(decid_gam,newdata=decid_train,type="response")
-decid_train$gam_class <- decid_train$gam_pred> 0.5
+gam_test$gam_pred <- predict(decid_gam,newdata=gam_test,type="response")
+gam_test$gam_class <- gam_test$gam_pred > 0.5
+gam_train$gam_pred <- predict(decid_gam,newdata=gam_train,type="response")
+gam_train$gam_class <- gam_train$gam_pred> 0.5
 
 ##Views predicted values and labels.
 par(mfrow=c(1,2))
-plot(decid_train$gam_pred,jitter(as.numeric(decid_train$Train_Decid),amount=0.1),
-     main="Training",xlab="Prediction",ylab="True Class",col=decid_train$gam_class+1)
-plot(decid_test$gam_pred,jitter(as.numeric(decid_test$Train_Decid),amount=0.1),
-     main="Testing",xlab="Prediction",ylab="True Class",col=decid_test$gam_class+1)
+plot(gam_train$gam_pred,
+     jitter(as.numeric(gam_train$Train_Decid),amount=0.1),
+     main="Training",xlab="Prediction",ylab="True Class",
+     col=gam_train$gam_class+1,pch=".")
+plot(gam_test$gam_pred,
+     jitter(as.numeric(gam_test$Train_Decid),amount=0.1),
+     main="Testing",xlab="Prediction",ylab="True Class",
+     col=gam_test$gam_class+1,pch=".")
 
 ##Computes confusion matrix and accuracy.
 evaluate_accuracy <- function(conf_mat){
@@ -152,26 +150,26 @@ write_csv(decid_test,"./Webinar5_Spatial_Prediction/data/decid_validation_data.c
 decid_rec <- recipe(decid_train) %>%
   update_role(Train_Decid,new_role="outcome") %>%
   #update_role(ID, new_role="ID") %>%
-  step_mutate(brightness_2017=(UG_rgbn_oct2017_1m_v1.1 + 
+  step_mutate(Brightness_2017=(UG_rgbn_oct2017_1m_v1.1 + 
                                  UG_rgbn_oct2017_1m_v1.2 +
                                  UG_rgbn_oct2017_1m_v1.3 +
                                  UG_rgbn_oct2017_1m_v1.4)/(255*4)) %>%
-  step_mutate(Pred_red2017_norm=UG_rgbn_oct2017_1m_v1.1 / (brightness_2017 * 255 * 4),
-              Pred_grn2017_norm=UG_rgbn_oct2017_1m_v1.2 / (brightness_2017 * 255 * 4),
-              Pred_blue2017_norm=UG_rgbn_oct2017_1m_v1.3 / (brightness_2017 * 255 * 4)) %>%
-  step_mutate(brightness_2019=(UG_rgbn_sept2019_1m_v1.1 + 
+  step_mutate(Red_norm_2017=UG_rgbn_oct2017_1m_v1.1 / (Brightness_2017 * 255 * 4),
+              Green_norm_2017=UG_rgbn_oct2017_1m_v1.2 / (Brightness_2017 * 255 * 4),
+              Blue_norm_2017=UG_rgbn_oct2017_1m_v1.3 / (Brightness_2017 * 255 * 4)) %>%
+  step_mutate(Brightness_2019=(UG_rgbn_sept2019_1m_v1.1 + 
                                  UG_rgbn_sept2019_1m_v1.2 +
                                  UG_rgbn_sept2019_1m_v1.3 +
                                  UG_rgbn_sept2019_1m_v1.4)/(255*4)) %>%
-  step_mutate(Pred_red2019_norm=UG_rgbn_sept2019_1m_v1.1 / (brightness_2019 * 255 * 4),
-              Pred_grn2019_norm=UG_rgbn_sept2019_1m_v1.2 / (brightness_2019 * 255 * 4),
-              Pred_blue2019_norm=UG_rgbn_sept2019_1m_v1.3 / (brightness_2019 * 255 * 4)) %>%
+  step_mutate(Red_norm_2019=UG_rgbn_sept2019_1m_v1.1 / (Brightness_2019 * 255 * 4),
+              Green_norm_2019=UG_rgbn_sept2019_1m_v1.2 / (Brightness_2019 * 255 * 4),
+              Blue_norm_2019=UG_rgbn_sept2019_1m_v1.3 / (Brightness_2019 * 255 * 4)) %>%
   #step_rm(contains("brightness_"))%>%
   step_normalize(contains("UG_")) %>%
-  step_normalize(contains("Pred_")) %>%
+  step_normalize(contains("norm")) %>%
   update_role(contains("UG_ndvi"),new_role="predictor") %>%
   update_role(contains("UG_canopy"),new_role="predictor") %>%
-  update_role(contains("Pred_"),new_role="predictor") %>%
+  update_role(contains("norm"),new_role="predictor") %>%
   prep(training = decid_train, retain = TRUE)
 
 ##Does this for the test data.
@@ -230,9 +228,9 @@ xgb_final_wf <- xgb_tune_wf %>%
 xgb_final_fit <- xgb_final_wf %>%
  fit(data=train_baked)
 
-##Writes model to an R object.
-saveRDS(xgb_final_fit,"./output/decid_xgb_trained.RDS")
-#xgb_final_fit <- readRDS("./output/decid_xgb_trained.RDS")
+##Writes tuned model to an R object.
+saveRDS(xgb_final_fit,"./Webinar5_Spatial_Prediction/output/xgb_fit.RDS")
+xgb_final_fit <- readRDS("./output/decid_xgb_trained.RDS")
 
 ## XGB Training results
 xgb_tr_results <- 
@@ -258,7 +256,7 @@ svm_tunespec <- svm_rbf(cost=tune(),
 ##Sets up grid search.
 svm_grid <- grid_regular(cost(range=c(-3,3)),
                          rbf_sigma(range=c(-3,0)),
-                         levels=5)
+                         levels=3)
 
 ##Sets up cross-validation.
 set.seed(57974)
@@ -288,8 +286,8 @@ svm_final_fit <- svm_final_wf %>%
  fit(data=train_baked)
 
 ##Writes model to an R object.
-#saveRDS(svm_final_fit,"./output/decid_svm_trained.RDS")
-svm_final_fit <- readRDS("./output/decid_svm_trained.RDS")
+saveRDS(svm_final_fit,"./Webinar5_Spatial_Prediction/output/svm_fit.RDS")
+svm_final_fit <- readRDS("./Webinar5_Spatial_Prediction/output/svm_fit.RDS")
 
 ## SVM Training results
 svm_tr_results <- 
@@ -304,6 +302,22 @@ svm_tr_results %>% accuracy(truth = Train_Decid, .pred_class)
 svm_tr_results %>% conf_mat(truth = Train_Decid, .pred_class)
 
 #### Validate models ####
+
+## Re-fits the GAM with updated predictors.
+gam_final_fit <- gam(Train_Decid ~ s(Red_norm_2017,Red_norm_2019)+
+                   s(Green_norm_2017,Green_norm_2019)+
+                   s(Blue_norm_2017,Blue_norm_2019)+
+                   s(UG_ndvi_oct2017_1m_v1,UG_ndvi_sept2019_1m_v2)+
+                   s(Brightness_2017,Brightness_2019)+
+                   s(UG_canopy_height_1m_v2),
+                 family="binomial",data=train_baked)
+summary(gam_final_fit)
+saveRDS(gam_final_fit,"./Webinar5_Spatial_Prediction/output/gam_fit.RDS")
+
+##GAM validation results.
+gam_pred <- predict(gam_final_fit,newdata=test_baked,type=
+                   "response") > 0.5
+
 
 ## XGB Validation results
 xgb_val_results <- 
@@ -328,6 +342,30 @@ svm_val_results %>% roc_auc(truth = Train_Decid, .pred_FALSE)
 svm_val_results %>% accuracy(truth = Train_Decid, .pred_class)
 svm_val_results %>% conf_mat(truth = Train_Decid, .pred_class)
 
+##Compares the three approaches.
+gam_conf_mat <- table(test_baked$Train_Decid,
+                      gam_pred)
+xgb_conf_mat <- table(xgb_val_results$Train_Decid,
+                      xgb_val_results$.pred_class)
+svm_conf_mat <- table(svm_val_results$Train_Decid,
+                      svm_val_results$.pred_class)
+
+model_eval <- rbind(evaluate_accuracy(gam_conf_mat),
+                  evaluate_accuracy(xgb_conf_mat),
+                  evaluate_accuracy(svm_conf_mat))
+model_eval$model <- c("GAM","XGB","SVM")
+model_eval
+
+##Quick ensemble.
+ens_class <- rowMeans(cbind(predict(gam_final_fit,newdata=test_baked,type=
+                                     "response"),
+                            predict(xgb_final_fit,new_data=test_baked,type="prob"),
+                            predict(svm_final_fit,new_data=test_baked,type="prob"))) > 0.5
+
+ens_conf_mat <- table(test_baked$Train_Decid,
+                      ens_class)
+evaluate_accuracy(ens_conf_mat)
+
 #### Large-scale prediction ####
 library(raster)
 library(TileManager)
@@ -338,17 +376,20 @@ library(RhpcBLASctl)
 
 ##Loads the tuned models from the previous step.
 gam_final_fit <- readRDS("./Webinar5_Spatial_Prediction/output/gam_fit.RDS")
-xgb_final_fit <- readRDS("./Webinar5_Spatial_Prediction/output/gam_fit.RDS")
-svm_final_fit <- readRDS("./Webinar5_Spatial_Prediction/output/gam_fit.RDS")
+xgb_final_fit <- readRDS("./Webinar5_Spatial_Prediction/output/xgb_fit.RDS")
+svm_final_fit <- readRDS("./Webinar5_Spatial_Prediction/output/svm_fit.RDS")
 
 ##Loads the extent of the predictors and selects a small test area.
 ug_bound_rast <- raster("/vsicurl/https://rmbl-sdp.s3.us-east-2.amazonaws.com/data_products/draft/UG_mask_1m_v1.tif")
+
+par(mfrow=c(1,1))
 plot(ug_bound_rast,maxpixels=10000)
 test_ext <- drawExtent()
 test_rast <- crop(ug_bound_rast,test_ext)
 
 ##Sets up tiles for large-scale processing.
-tiles <- tileScheme(test_rast,tiledim=c(500,500),cells=TRUE,buffer=0)
+tiles <- tileScheme(test_rast,tiledim=c(1000,1000),cells=TRUE,buffer=0)
+plot(tiles,add=TRUE)
 
 ##Set the number of threads for linear algebra (fastest is usually 1)
 threads <- 1
@@ -386,7 +427,7 @@ out_tiles <- foreach(i=1:length(tiles@buffs),
    ##checks to see if output files exist.
    tile_center <- paste("w",round(mean(tile_extent@xmin,tile_extent@xmax)),"_",
                         "n",round(mean(tile_extent@ymin,tile_extent@ymax)),sep="")
-   outname <- paste("./output/tiles_prob/decid_prob_tiles_",tile_center,".tif",sep="")
+   outname <- paste("Webinar5_Spatial_Prediction/output/tiles_prob/decid_prob_tiles_",tile_center,".tif",sep="")
    #outname2 <- paste("./output/tiles_gam_ht/decid_class_tiles_",tile_center,".tif",sep="")
    
    ##Removes tile scheme to save memory.
@@ -438,7 +479,7 @@ out_tiles <- foreach(i=1:length(tiles@buffs),
      svm_prob_na[!missing] <- round(predict(svm_final_fit,new_data=pred_baked,type="prob")$'.pred_TRUE' * 10000)
      print(paste("SVM prediction complete..."))
      gam_prob_na <- rep(NA,length(missing))
-     gam_prob_na[!missing] <- round(predict(gam_final_fit,newdata=gam_baked,type="response",block.size=100000) * 10000)
+     gam_prob_na[!missing] <- round(predict(gam_final_fit,newdata=pred_baked,type="response",block.size=100000) * 10000)
      print(paste("GAM prediction complete..."))
      
      ##Reshapes into a raster object.
@@ -476,7 +517,13 @@ total_elapsed
 ##Mosaics output tiles together.
 library(gdalUtils)
 
+
 ##Builds a list of files.
-tile_paths <- list.files("./Webinar5_Spatial_Prediction/output/tiles_prob/",
-                         pattern=".tif$")
+tile_paths <- list.files("~/code/SpatialDataScienceWebinars2020/Webinar5_Spatial_Prediction/output/tiles_prob/",
+                         pattern=".tif$",full.names = TRUE)
 gdalbuildvrt(tile_paths,"./Webinar5_Spatial_Prediction/output/tile_mosaic.vrt")
+
+##Reads in as an R object.
+pred_mosaic <- brick("./Webinar5_Spatial_Prediction/output/tile_mosaic.vrt")
+plot(pred_mosaic)
+
